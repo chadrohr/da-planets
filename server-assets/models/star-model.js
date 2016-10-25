27ea4f02902a5552d950914ca6c9ff6a6a2bd966
Chad Rohr <chadrohr@yahoo.com>
2016-10-24 21:24:12 -0600

    EOD Moons are working

--- server-assets/models/star-model.js
+++ server-assets/models/star-model.js
@@ -1,47 +1,51 @@
 let dataAdapter = require('./data-adapter'),
-  uuid = dataAdapter.uuid,
-  schemator = dataAdapter.schemator,
-  DS = dataAdapter.DS,
-  formatQuery = dataAdapter.formatQuery;
+    uuid = dataAdapter.uuid,
+    schemator = dataAdapter.schemator,
+    DS = dataAdapter.DS,
+    formatQuery = dataAdapter.formatQuery;
 
 let Star = DS.defineResource({
-  name: 'star',
-  endpoint: 'stars',
-  filepath: __dirname + '/../data/stars.db',
-  relations: {
-    belongsTo: {
-      galaxy: {
-        localField: 'galaxy',
-        localKey: 'galaxyId'
-      }
-    },
-    hasMany: {
-      planet: {
-        localField: 'planets',
-        foreignKey: 'starId'
-      }
+    name: 'star',
+    endpoint: 'stars',
+    filepath: __dirname + '/../data/stars.db',
+    relations: {
+        belongsTo: {
+            galaxy: {
+                localField: 'galaxy',
+                localKey: 'galaxyId'
+            }
+        },
+        hasMany: {
+            planet: {
+                localField: 'planets',
+                foreignKey: 'starId'
+            },
+            moon: {
+                localField: 'moons',
+                foreignKey: 'starId'
+            }
+        }
     }
-  }
 })
 
 
 function create(star, cb) {
-  // Use the Resource Model to create a new star
-  Star.create({ id: uuid.v4(), name: star.name, galaxyId: star.galaxyId}).then(cb).catch(cb)
+    // Use the Resource Model to create a new star
+    Star.create({ id: uuid.v4(), name: star.name, galaxyId: star.galaxyId }).then(cb).catch(cb)
 }
 
 function getAll(query, cb) {
-  //Use the Resource Model to get all Galaxies
-  Star.findAll({}).then(cb).catch(cb)
+    //Use the Resource Model to get all Galaxies
+    Star.findAll({}).then(cb).catch(cb)
 }
 
 function getById(id, query, cb) {
-  // use the Resource Model to get a single star by its id
-  Star.find(id, formatQuery(query)).then(cb).catch(cb)
+    // use the Resource Model to get a single star by its id
+    Star.find(id, formatQuery(query)).then(cb).catch(cb)
 }
 
 module.exports = {
-  create,
-  getAll,
-  getById
+    create,
+    getAll,
+    getById
 }
