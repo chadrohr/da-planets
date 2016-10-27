a5fe0d921453f9481dd1151f5cb3cb55e1529a4d
Chad Rohr <chadrohr@yahoo.com>
2016-10-27 12:02:52 -0600

    whoops

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 var constants = {
     COWABUNGA: 'COWABUNGA',
     GIVEMEPLANETS: 'GIVE ME PLANETS',
-    PLANETSRECIEVED: 'PLANETS RECIEVED',
+    PLANETSRECIEVED: 'PLANETSRECIEVED',
     social: {
         facebook: 'boisecodeworks',
         twitter: 'boisecodeworks',
